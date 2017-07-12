f1214c5729f397a07ea04a9af1b205807e9a4c73
samcargalbley <samcargalbley@gmail.com>
2017-07-11 21:04:25 -0400

    contact us page

---
+++ client/contactus.js
@@ -0,0 +1,10 @@
+const contacts = [
+  {name: "Sam Cargal-Bley", eml: "samcargalbley@gmail.com"},
+  {name: "James Mendel", eml: "jrm.mendel@gmail.com"},
+  {name: "Amen Tesfaye", eml: "amentesfaye15@gmail.com"},
+  {name: "Vinnyak Sharma", eml: "vsharma3600@gmail.com"},
+]
+
+Template.contactus.helpers({
+  contactInfo: contacts
+})
