a251721f2325b6a338dfec08c050a31199b8a918
samcargalbley <samcargalbley@gmail.com>
2017-07-12 08:52:15 -0400

    Removed contact

--- client/contactus.js
+++ client/contactus.js
@@ -3,7 +3,6 @@ const contacts = [
   {name: "James Mendel", eml: "jrm.mendel@gmail.com"},
   {name: "Amen Tesfaye", eml: "amentesfaye15@gmail.com"},
   {name: "Vinnyak Sharma", eml: "vsharma3600@gmail.com"},
-  {name: "Keyser Söze", eml: "ksoze@hotmail.com"},
 ]
 
 Template.contactus.helpers({
